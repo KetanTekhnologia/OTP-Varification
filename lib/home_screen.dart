6935109ccb0c050348fbb30a1cf475a42b07fa73
ketan1435 <shendkarketan1435@gmail.com>
2024-04-16 18:16:33 +0530

    16-04

---
+++ lib/home_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(child: Text("HomeScreen",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25,color: Colors.black),))
+        ],
+      ),
+    );
+  }
+}
